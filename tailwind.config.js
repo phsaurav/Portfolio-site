e97369ea52ef7bb6bf87c9ea60219353dbc51727
PH Saurav <phsaurav@PHs-Mac.local>
2021-11-29 03:58:51 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		letterSpacing: {
+			wide: ".01em",
+			wider: ".05em",
+			widest: ".5em",
+		},
+		screens: {
+			sm: "480px",
+			md: "1024px",
+			lg: "1080px",
+			xl: "1440px",
+		},
+		colors: {
+			green: colors.green,
+			black: colors.black,
+			white: colors.white,
+			gray: colors.trueGray,
+			bluegray: colors.blueGray,
+			indigo: colors.indigo,
+			red: colors.rose,
+			yellow: colors.amber,
+			orange: colors.orange,
+			blue: colors.blue,
+			lime: colors.lime,
+			brand: {
+				1: "#1e1e1e",
+				2: "#666666",
+				3: "#000000",
+				4: "#ffffff",
+				5: "#A1A1AA",
+			},
+		},
+		fontFamily: {},
+		fontSize: {
+			xs: ".75rem",
+			sm: ".875rem",
+			tiny: ".875rem",
+			base: "1rem",
+			lg: "1.125rem",
+			xl: "1.25rem",
+			"2xl": "1.5rem",
+			"3xl": "1.875rem",
+			"4xl": "2.25rem",
+			"5xl": "3rem",
+			"6xl": "4rem",
+			"7xl": "5rem",
+			"8xl": "7rem",
+			"9xl": "9rem",
+			"10xl": "12rem",
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
