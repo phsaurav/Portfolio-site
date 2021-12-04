3d6c07edae504c7e351b7d1e5a71f077e891df27
PH Saurav <phsaurav29@gmail.com>
2021-12-05 01:14:55 +0600

    Header Fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 		pointerEvents: "none",
 	};
 	return (
-		<div className='bg-brand-1 pt-5'>
+		<div className='bg-brand-1 pt-5 pb-3'>
 			<nav className='relative z-20'>
 				<div className='container mx-auto'>
 					<div className='text-black'>
