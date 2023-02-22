378f7a9b1804858db1ece621c50e6ccc556a8cfe
PH Saurav <phsaurav29@gmail.com>
2023-02-22 18:47:17 +0600

    Animation added to skills

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./containers/Home/Home";
-import AllProjects from "./containers/AllProjects/AllProjects";
-import ProjectInfo from "./components/ProjectInfo/ProjectInfo";
-import AboutMe from "./containers/Home/AboutMe/AboutMe";
-import Blogs from "./containers/Blogs/Blogs";
-import Blog from "./containers/Blogs/Blog/Blog";
-import AllBlog from "./containers/Blogs/AllBlog/AllBlog";
 import { useEffect } from "react";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ProjectInfo from "./components/ProjectInfo/ProjectInfo";
+import AllProjects from "./containers/AllProjects/AllProjects";
+import AllBlog from "./containers/Blogs/AllBlog/AllBlog";
+import Blog from "./containers/Blogs/Blog/Blog";
+import Blogs from "./containers/Blogs/Blogs";
+import AboutMe from "./containers/Home/AboutMe/AboutMe";
+import Home from "./containers/Home/Home";
 
 function App() {
-	useEffect(() => {
-		AOS.init();
-	}, []);
-	return (
-		<div className='App'>
-			<Router>
-				<Routes>
-					<Route path='/' element={<Navigate replace to='/home' />}></Route>
-					<Route path='/home' element={<Home></Home>}></Route>
-					<Route path='/allprojects' element={<AllProjects></AllProjects>}></Route>
-					<Route path='/aboutme' element={<AboutMe></AboutMe>}></Route>
-					<Route path='/blogs' element={<Blogs></Blogs>}>
-						<Route path='/blogs' element={<AllBlog />} />
-						<Route path=':key' element={<Blog />} />
-					</Route>
-					<Route path='/project/:id' element={<ProjectInfo></ProjectInfo>}></Route>
-				</Routes>
-			</Router>
-		</div>
-	);
+    useEffect(() => {
+        AOS.init({ offset: 10, delay: 500 });
+    }, []);
+    return (
+        <div className='App'>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Navigate replace to='/home' />}></Route>
+                    <Route path='/home' element={<Home></Home>}></Route>
+                    <Route path='/allprojects' element={<AllProjects></AllProjects>}></Route>
+                    <Route path='/aboutme' element={<AboutMe></AboutMe>}></Route>
+                    <Route path='/blogs' element={<Blogs></Blogs>}>
+                        <Route path='/blogs' element={<AllBlog />} />
+                        <Route path=':key' element={<Blog />} />
+                    </Route>
+                    <Route path='/project/:id' element={<ProjectInfo></ProjectInfo>}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/containers/Home/Skills/FrontendSkills/FrontendSkills.js
+++ src/containers/Home/Skills/FrontendSkills/FrontendSkills.js
@@ -6,58 +6,86 @@ const FrontendSkills = () => {
             <div className='flex flex-wrap justify-center gap-2'>
                 <img
                     src='https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='React'
                 />
                 <img
                     src='https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Next JS'
                 />
                 <img
                     src='https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Flutter'
                 />
                 <img
                     src='https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Redux'
                 />
                 <img
                     src='https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='TailwindCSS'
                 />
                 <img
                     src='https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='MUI'
                 />
                 <img
                     src='https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='React Router'
                 />
                 <img
                     src='https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='TypeScript'
                 />
                 <img
                     src='https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='JavaScript'
                 />
                 <img
                     src='https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Dart'
                 />
                 <img
                     src='https://img.shields.io/badge/cypress-%2317202C.svg?style=for-the-badge&logo=cypress&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Cypress'
                 />
                 <img
                     src='https://img.shields.io/badge/Testing%20Library-%23E33332.svg?style=for-the-badge&logo=Testing%20Library&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Testing Library'
                 />
                 <img
                     src='https://img.shields.io/badge/Riverpod-%230081CB.svg?style=for-the-badge'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Riverpod'
                 />
                 <img
                     src='https://img.shields.io/badge/Mockito-%2368BC71.svg?style=for-the-badge&logo=Mocha&logoColor=white'
+                    data-aos='zoom-in-up'
+                    data-aos-duration='1000'
                     alt='Mockito'
                 />
             </div>
